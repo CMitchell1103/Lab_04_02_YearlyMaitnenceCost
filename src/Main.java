1342dd73336e2a7608bb3a4f706a977fbab7a434
CMitchell1103 <mitch3cd@mail.uc.edu>
2024-09-30 18:01:24 -0400

    Final product

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        double springCost;
+        double summerCost;
+        double winterCost;
+        double fallCost;
+
+        System.out.println("What is the Spring maintenance cost: ");
+            springCost = input.nextDouble();
+
+        System.out.println("What is the Summer maintenance cost: ");
+            summerCost = input.nextDouble();
+
+        System.out.println("What is the Winter maintenance cost: ");
+            winterCost = input.nextDouble();
+
+        System.out.println("What is te Fall maintenance cost: ");
+            fallCost = input.nextDouble();
+
+        System.out.println("The maintenance costs are: ");
+        System.out.println("The Summer cost is $ " + summerCost);
+        System.out.println("The Winter cost is $ " + winterCost);
+        System.out.println("The Fall cost is $ " + fallCost);
+        System.out.println("The Spring cost is $ " + springCost);
+        System.out.println("The total maintenance cost is $ " + springCost + summerCost + winterCost + fallCost);
     }
 }
